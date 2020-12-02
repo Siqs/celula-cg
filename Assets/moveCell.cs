9664b5c007c351b4f46d43f94fc6c67675987d86
Siqs <luc.siqueira94@gmail.com>
2020-12-01 21:18:39 -0300

    started project

---
+++ Assets/moveCell.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class moveCell : MonoBehaviour
+{
+    public float speed;
+    private int points;
+    public Text pointsLabel;
+    private Rigidbody rb;
+
+    void Start() {
+        points = 0;
+        pointsLabel.text = "Pontos: " + points.ToString ();
+        rb = GetComponent<Rigidbody> ();
+    }
+
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 1, moveVertical);
+
+        rb.AddForce (movement * speed);        
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Enemy")) {
+            pointsLabel.text = "Game Over";
+            Destroy(this.gameObject);
+            
+        } else if (other.gameObject.CompareTag("Food")) {
+            Destroy(other.gameObject);
+            points++;
+            pointsLabel.text = "Pontos: " + points.ToString ();
+        }
+    }
+}
---
+++ Assets/moveEnemy.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveEnemy : MonoBehaviour {
+	
+	public float speed = 5f;
+	public float minDist = 1f;
+	public Transform target;
+
+	void Start () 
+	{
+		if (target == null) {
+			if (GameObject.FindWithTag ("Player")!=null) {
+				target = GameObject.FindWithTag ("Player").GetComponent<Transform>();
+			}
+		}
+	}
+	
+	void Update () {
+		if (target == null)
+			return;
+
+		transform.LookAt(target);
+
+		float distance = Vector3.Distance(transform.position,target.position);
+
+		if(distance > minDist)	
+			transform.position += transform.forward * speed * Time.deltaTime;	
+	}
+
+	public void SetTarget(Transform newTarget) {
+		target = newTarget;
+	}
+}
